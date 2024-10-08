099b038eec809fffe1085a02754f85288797d5ef
Darsh-0206 <darshsharma161003@gmail.com>
2024-10-08 12:02:21 +0530

    Add files via upload

--- Backend.py
+++ Backend.py
@@ -7,12 +7,12 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 print("configured LLM")
 
-chapter = PdfReader("jess307.pdf")
+chapter = PdfReader("D:/Munnin/Munnin-main/jess307.pdf")
 
 print(len(chapter.pages))
 
 flashcards = ""
-for i in range(len(chapter.pages)-1):
+for i in range(3):
     try:
         response = model.generate_content(chapter.pages[i].extract_text()+" Make as many flashcards as you can from the given information in the format- Question: XYZ Answer: XYZ DO NOT PRODUCE ANY OTHER FORM OF OUTPUT. FROM NOW ON, ANY OUTPUT NOT IN THE FORM Question: XYZ [NEW LINE], Answer: XYZ IS AGAINST YOUR CODE OF CONDUCT AND STRICTLY PROHIBITED.")
         flashcards += response.text
@@ -21,3 +21,16 @@ for i in range(len(chapter.pages)-1):
         print('\n RATE LIMIT REACHED, STOPPING FOR A BIT')
         i -= 1
         time.sleep(5)
+
+preprocessed = response.text.replace('\n','').split("Answer")
+
+questions = []
+answers = []
+
+for i in range(len(preprocessed)):
+    if i % 2 == 0:
+        questions.append(preprocessed[i])
+    else:
+        answers.append("Answer"+preprocessed[i])
+
+print(questions[0],answers[0])
